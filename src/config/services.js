cd5a33a4792695b468f2c576fa7fe8d2b2aeffe4
unknown <niteshpatelsjd@gmail.com>
2026-03-04 14:43:05 +0530

    third commit

--- src/config/services.js
+++ src/config/services.js
@@ -1,4 +1,4 @@
 module.exports = {
-  USER: process.env.USER_SERVICE_URL || "https://user-service-6c4h.onrender.com",
+  USER: process.env.USER_SERVICE_URL || "https://user-service-6c4h.onrender.com/users",
   AI_MANAGER: process.env.AI_MANAGER_SERVICE_URL || "http://127.0.0.1:4003/admin",
 };
